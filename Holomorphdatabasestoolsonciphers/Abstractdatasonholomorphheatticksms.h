de459bc084bae8a98dca05739193cbf9199980a7
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-09 09:11:13 +0100

    Create Abstractdatasonholomorphheatticksms.h

---
+++ Holomorphdatabasestoolsonciphers/Abstractdatasonholomorphheatticksms.h
@@ -0,0 +1,3 @@
+//we can have a better function estimate if we merge all those databases.
+//then signals qfe no longer ciphers. but we have to take 0,6 or 6,2 signals (planck to signal).
+//so sparse databases are useless if we..
