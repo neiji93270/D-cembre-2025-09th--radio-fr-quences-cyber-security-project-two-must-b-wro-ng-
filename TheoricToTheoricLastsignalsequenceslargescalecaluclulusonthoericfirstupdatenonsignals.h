fbfb018b77712a06431f8819f18bde2ebf660404
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-09 11:55:24 +0100

    Create TheoricToTheoricLastsignalsequenceslargescalecaluclulusonthoericfirstupdatenonsignals.h

---
+++ TheoricToTheoricLastsignalsequenceslargescalecaluclulusonthoericfirstupdatenonsignals.h
@@ -0,0 +1,3 @@
+//we can negate ciphers if there are close signals enough close to rpobes.. Then, a sparse databse cannot handle that.
+//we would better to keep track of non related all bytes dtas to convert them into ciphers if the thoeries of theories we take could
+//track differential evolutions. This is also a virtualizer of databses if the next theory is close to other theories related to sparse models.
